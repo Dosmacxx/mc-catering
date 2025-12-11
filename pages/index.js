f4319ab587eef91c31f9e767363354396138cf61
Dosmacxx <lautarogaunaprex@gmail.com>
2025-12-11 15:48:20 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,33 +28,46 @@ export default function Home() {
   }, []);
 
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        overflow: "hidden",
-        backgroundImage: `url(${imagenes[index]})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        transition: "background-image 1s ease-in-out",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-start",
-        paddingLeft: "5vw",
-      }}
-    >
-      <h1
-        style={{
-          fontSize: "clamp(1.8rem, 5vw, 4rem)",
-          color: "white",
-          fontWeight: "900",
-          textShadow: "0px 3px 12px rgba(0,0,0,0.8)",
-        }}
-      >
-        MC CATERING
-      </h1>
-    </div>
-  );
-}
+ <div
+  style={{
+    width: "100vw",
+    height: "100vh",
+    overflow: "hidden",
+    position: "relative",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingLeft: "5vw",
+  }}
+>
+  {/* Imagen de fondo controlada */}
+  <img
+    src={imagenes[index]}
+    alt="MC Catering"
+    style={{
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      width: "120%",        // ⭐ evita que la foto se vea gigante o pixelada
+      height: "120%",       // ⭐ mantiene proporción sin deformar
+      objectFit: "cover",   // ⭐ asegura que la foto siempre se vea bien
+      objectPosition: "center",
+      transform: "translate(-50%, -50%)",
+      filter: "brightness(0.65)",  // ⭐ oscurece para que el texto se lea siempre
+      transition: "opacity 1s ease-in-out",
+      zIndex: -1,
+    }}
+  />
 
+  <h1
+    style={{
+      fontSize: "clamp(2rem, 5vw, 4rem)",
+      color: "white",
+      fontWeight: "900",
+      textShadow: "0px 3px 15px rgba(0,0,0,0.9)",
+      zIndex: 10,
+    }}
+  >
+    MC CATERING
+  </h1>
+</div>
